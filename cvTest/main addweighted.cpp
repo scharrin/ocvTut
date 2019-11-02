1e2c594706f82a40a999a6717421efa36a9bbb2c
ddd <dedeten2@naver.com>
2019-11-03 00:49:02 +0900

    Add bitoperation part tutorial

---
+++ cvTest/main addweighted.cpp
@@ -0,0 +1,45 @@
+/// merge images by addweighted
+
+///
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+Mat
+matFlower
+, matFlower2
+, result
+;
+
+int main(int argc, const char * argv[]) {
+    
+    namedWindow("w");
+    string filePath = "/Users/bro/Downloads/";
+    string img1FileName = "flower1.jpg";
+    string img2FileName = "flower2.jpg";
+
+    matFlower = imread(filePath + img1FileName);
+    matFlower2 = imread(filePath + img2FileName);
+    
+    createTrackbar("merge", "w", 0, 100);
+    
+    int int_keyInput, trackbarValue;
+    
+    while (1) {
+        trackbarValue = getTrackbarPos("merge", "w");
+        addWeighted(matFlower, 0.01 * ( 100 - trackbarValue ), matFlower2, 0.01 * trackbarValue, 0.0, result);
+        
+        imshow("w", result);
+        int_keyInput = waitKey(1);
+        if ( int_keyInput == 27 ) break;    /// esc key
+    }
+    destroyAllWindows();
+    
+    return 0;
+}
+
+
+
---
+++ cvTest/main maskAndMergeLogo.cpp
@@ -0,0 +1,88 @@
+/// merge images by addweighted
+
+///
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+Mat
+matFlower
+, matImgLogo
+, matImgLogoBG
+, matImgLogoGray
+, result
+, imgMask
+, imgMaskInv
+, matMorphKernel
+, roiOfBgImg
+;
+
+int
+thresholdColorLimit = 255
+, thresholdMorphSizeLimit
+, thresholdColorInitialValue = 10
+, thresholdMorphSizeInitialValue = 0
+;
+
+int main(int argc, const char * argv[]) {
+    
+    namedWindow("w");
+    string filePath = "/Users/bro/Downloads/";
+    string img1FileName = "flower1.jpg";
+    //    string img2FileName = "flower2.jpg";
+    //    string img2FileName = "/Users/bro/Desktop/dev/ocv/cvTest/cvTest/src/key-circular-button.png";
+    string img2FileName = "/Users/bro/Downloads/logo_2.png";
+    matFlower = imread(filePath + img1FileName);
+    //    matImgLogo = imread(img2FileName, -1);
+    //    Mat channels[4];
+    //    split(matImgLogo, channels);
+    //    bitwise_not(channels[3], channels[3]);
+    //    imshow("logo", channels[3]);
+      
+    matImgLogo = imread(img2FileName);
+    //    matImgLogoBG.copySize(matImgLogo);
+    //    matImgLogoBG.setTo(255);
+    //    imshow("logo", matImgLogo);
+  
+    Rect roi(0, 0, matImgLogo.cols, matImgLogo.rows);
+    roiOfBgImg = matFlower(roi);
+    cvtColor(matImgLogo, matImgLogoGray, COLOR_RGB2GRAY);
+    
+//    if ( matImgLogoGray.rows > matImgLogoGray.cols ) thresholdMorphSizeLimit = matImgLogoGray.cols + 1;
+//    else thresholdMorphSizeLimit = matImgLogoGray.rows + 1;
+    
+    createTrackbar("thres", "w", &thresholdColorInitialValue, thresholdColorLimit);
+//    createTrackbar("morphSize", "w", &thresholdMorphSizeInitialValue, thresholdMorphSizeLimit);
+    
+    int int_keyInput, trackbarValue, trackbarMorphValue;
+    
+    while (1) {
+        trackbarValue = getTrackbarPos("thres", "w");
+//        trackbarMorphValue = getTrackbarPos("morphSize", "w") + 1;
+        threshold(matImgLogoGray, imgMask, trackbarValue, thresholdColorLimit, THRESH_BINARY);
+//        matMorphKernel = Mat::ones(trackbarMorphValue, trackbarMorphValue, CV_8U);
+//        morphologyEx(imgMask, imgMask, MORPH_CLOSE, matMorphKernel);
+        
+        bitwise_not(imgMask, imgMaskInv);
+
+//        matImgLogo.copyTo(matImgLogo, imgMask);
+//        roiOfBgImg.copyTo(roiOfBgImg, imgMaskInv);
+        
+        bitwise_and(matImgLogo, matImgLogo, matImgLogo, imgMask);
+        bitwise_and(roiOfBgImg, roiOfBgImg, roiOfBgImg, imgMaskInv);
+        
+        add(roiOfBgImg, matImgLogo,  result);
+//        imshow("w1", result);
+
+        matFlower(roi) = result;
+        imshow("w", matFlower);
+        int_keyInput = waitKey(1);
+        if ( int_keyInput == 27 ) break;    /// esc key
+    }
+    destroyAllWindows();
+    
+    return 0;
+}
